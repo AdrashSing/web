fa9a003d7697d4d677884efd3266e0dc382d76a4
Adarsh Singh <adrash.code@gmail.com>
2025-11-29 17:21:45 +0530

    Add files via upload

---
+++ video.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const video = () => {
+  return (
+    <div className='h-full w-full'>
+         <video className='h-full w-full object-cover' autoPlay muted loop src="https://www.pexels.com/download/video/3783928/"></video>
+    </div>
+  )
+}
+
+export default video
